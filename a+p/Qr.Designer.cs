79bfca69c1d9527cf0fd0056b3f960dead627098
Jona Cañedo Islas <jonathancanedo88@gmail.com>
2023-08-30 13:39:58 -0600

    Agregar archivos de proyecto.

---
+++ a+p/Qr.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace a_p
+{
+    partial class Qr
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Qr));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.iconMin = new System.Windows.Forms.PictureBox();
+            this.iconCerrar = new System.Windows.Forms.PictureBox();
+            this.pbIcon = new System.Windows.Forms.PictureBox();
+            this.pbQr = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.iconMin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(12, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 73;
+            this.label1.Text = "Asistencia Qr";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.label3.ForeColor = System.Drawing.Color.LightGray;
+            this.label3.Location = new System.Drawing.Point(-2, -1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(391, 31);
+            this.label3.TabIndex = 72;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label3_MouseMove);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHora.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblHora.Location = new System.Drawing.Point(249, 80);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(57, 20);
+            this.lblHora.TabIndex = 76;
+            this.lblHora.Text = "label2";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFecha.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblFecha.Location = new System.Drawing.Point(230, 46);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(57, 20);
+            this.lblFecha.TabIndex = 76;
+            this.lblFecha.Text = "label2";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // iconMin
+            // 
+            this.iconMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.iconMin.BackColor = System.Drawing.Color.RoyalBlue;
+            this.iconMin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconMin.Image = global::a_p.Properties.Resources.mi;
+            this.iconMin.Location = new System.Drawing.Point(342, 4);
+            this.iconMin.Name = "iconMin";
+            this.iconMin.Size = new System.Drawing.Size(18, 20);
+            this.iconMin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconMin.TabIndex = 75;
+            this.iconMin.TabStop = false;
+            this.iconMin.Click += new System.EventHandler(this.iconMin_Click);
+            // 
+            // iconCerrar
+            // 
+            this.iconCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.iconCerrar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.iconCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.iconCerrar.Image = global::a_p.Properties.Resources.ce;
+            this.iconCerrar.Location = new System.Drawing.Point(366, 4);
+            this.iconCerrar.Name = "iconCerrar";
+            this.iconCerrar.Size = new System.Drawing.Size(18, 20);
+            this.iconCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.iconCerrar.TabIndex = 74;
+            this.iconCerrar.TabStop = false;
+            this.iconCerrar.Click += new System.EventHandler(this.iconCerrar_Click);
+            // 
+            // pbIcon
+            // 
+            this.pbIcon.BackColor = System.Drawing.Color.Transparent;
+            this.pbIcon.Image = global::a_p.Properties.Resources.a_pF;
+            this.pbIcon.Location = new System.Drawing.Point(12, 43);
+            this.pbIcon.Name = "pbIcon";
+            this.pbIcon.Size = new System.Drawing.Size(151, 150);
+            this.pbIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbIcon.TabIndex = 0;
+            this.pbIcon.TabStop = false;
+            // 
+            // pbQr
+            // 
+            this.pbQr.Location = new System.Drawing.Point(134, 127);
+            this.pbQr.Name = "pbQr";
+            this.pbQr.Size = new System.Drawing.Size(217, 212);
+            this.pbQr.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbQr.TabIndex = 0;
+            this.pbQr.TabStop = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 3600000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Qr
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(387, 366);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.iconMin);
+            this.Controls.Add(this.iconCerrar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbIcon);
+            this.Controls.Add(this.pbQr);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Qr";
+            this.Opacity = 0.85D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Qr_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.iconMin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.iconCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbQr;
+        private System.Windows.Forms.PictureBox iconMin;
+        private System.Windows.Forms.PictureBox iconCerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pbIcon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
---
+++ a+p/Qr.cs
@@ -0,0 +1,125 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static QRCoder.PayloadGenerator;
+using MySql.Data.MySqlClient;
+
+namespace a_p{
+    public partial class Qr : Form{
+        public string a="";
+        public int xClick = 0, yClick = 0;
+        public Qr(){
+            InitializeComponent();
+        }
+        private void Qr_Load(object sender, EventArgs e){
+            lblFecha.Text = DateTime.Now.ToShortDateString();
+            qr();
+            Envios();
+        }
+        private void timer1_Tick(object sender, EventArgs e){
+            lblHora.Text = DateTime.Now.ToString("HH:mm");
+        }
+        private void timer2_Tick(object sender, EventArgs e){
+            qr();
+        }
+        private void qr(){
+            QRCodeGenerator qrGenerador = new QRCodeGenerator();
+            QRCodeData qrdat = qrGenerador.CreateQrCode(lblHora.Text = DateTime.Now.ToString("HH:mm"), QRCodeGenerator.ECCLevel.H);
+            QRCode codigo = new QRCode(qrdat);
+
+            Bitmap qrI = codigo.GetGraphic(9, Color.Blue, Color.Empty, true);
+            pbQr.Image = qrI;
+        }
+        private void iconCerrar_Click(object sender, EventArgs e){
+            DialogResult res = MessageBox.Show("¿ Realmente desea cerrar la aplicación ?", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (res == DialogResult.Yes){
+                Environment.Exit(0);
+            }else { }
+        }
+        private void iconMin_Click(object sender, EventArgs e){
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void label3_MouseMove(object sender, MouseEventArgs e){
+            if (e.Button != MouseButtons.Left){
+                xClick = e.X; yClick = e.Y;
+            }else{
+                this.Left = this.Left + (e.X - xClick); this.Top = this.Top + (e.Y - yClick);
+            }
+        }
+        private void Envios(){
+            char[] cadena = lblHora.Text.ToCharArray();
+            a = cadena[0] + "" + cadena[1];
+
+            if (Convert.ToInt32(a) == 13){
+                try{
+                    MySqlConnection con = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=a+p");
+                    int c = Contar();
+                    List<string> macs = new List<string>();
+
+                    con.Open();
+                    string Query = ("select mac from trab");
+                    MySqlCommand cmd = new MySqlCommand(Query, con);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read()){
+                        macs.Add(Convert.ToString(reader["mac"]));
+                    }
+                    con.Close();
+                    for (int i = 0; i < c; i++){
+                        if (!existe(macs[i],lblFecha.Text)){
+                            string consu = "INSERT INTO registros (mac,fecha,entrada,salida) VALUES (@mac,@fecha,@entrada,@salida)";
+                            cmd = new MySqlCommand(consu, con);
+                            
+                            #region Datos
+                            cmd.Parameters.AddWithValue("@mac", macs[i]);
+                            cmd.Parameters.AddWithValue("@fecha", lblFecha.Text);
+                            cmd.Parameters.AddWithValue("@entrada", "0");
+                            cmd.Parameters.AddWithValue("@salida", "0");                            
+                            #endregion
+
+                            con.Open();
+                            cmd.ExecuteNonQuery();
+                            con.Close();
+                        }else { }
+                    }
+                }catch { }
+            }else{}
+        }
+        private int Contar(){
+            int s = 0;
+            try{
+                MySqlConnection con = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=a+p");
+                con.Open();
+                string Query = ("select count(mac) from trab");
+                MySqlCommand cmd = new MySqlCommand(Query, con);
+                s = Convert.ToInt32(cmd.ExecuteScalar());
+                con.Close();
+            }catch { }
+            return s;
+        }
+        private void timer3_Tick(object sender, EventArgs e){
+            Envios();
+        }
+        private bool existe(string mac, string fecha){
+            MySqlConnection con = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=a+p");
+            string query = "SELECT COUNT(*) FROM registros WHERE mac=@mac AND fecha=@fecha";
+            MySqlCommand cmd = new MySqlCommand(query, con);
+            cmd.Parameters.AddWithValue("mac", mac);
+            cmd.Parameters.AddWithValue("fecha", fecha);
+            con.Open();
+            int count = Convert.ToInt32(cmd.ExecuteScalar());
+            if (count == 0){
+                return false;
+            }else{
+                return true;
+            }
+        }
+            
+    }
+}
